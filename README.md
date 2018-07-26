ea88fa421dbceaaf2224a2890249cdf06de03c4b
Rohit Ner <rohitner1@gmail.com>
2018-07-26 23:51:29 +0530

    Refine UI Update README Ruin people

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # ERP-BDAY
 get birth date of your best friend for several purposes from ERP
+
+
+# Usage
+```python
+python bday.py
+# Enter the rollno of your best friend.: 16MA200XX
+# Enter the possible birth year.       : 1998
+# 01-01-1998
+```
+
+Use it for
+- Reminding your friend for the b'day treat.
+
+Dont use it for
+- Checking his/her cgpa.
+
+# License
+MIT License
--- bday.py
+++ bday.py
@@ -1,28 +1,52 @@
-import re
+import sys
 from mechanize import Browser
 from HTMLParser import HTMLParser
 from datetime import timedelta, date
+
+def spinning_cursor():
+  while True:
+    for cursor in '|/-\\':
+      yield cursor
 
 def daterange(start_date, end_date):
     for n in range(int ((end_date - start_date).days)):
         yield start_date + timedelta(n)
 
-arr = []
+def get_bday(rollno, start_date, end_date):
+  spinner = spinning_cursor()
+  flag = 1
+  for single_date in daterange(start_date, end_date):
+    br = Browser()
+    br.open("https://erp.iitkgp.ac.in/StudentPerformanceV2/auth/login.htm")
+    br.select_form(name="loginform")
+    br.form.set_all_readonly(False)
 
-start_date = date(1998, 1, 1)
-end_date   = date(1998,12,31)
+    br["rollno"] = rollno
+    br["dob"] = single_date.strftime("%d-%m-%Y")
 
-for single_date in daterange(start_date, end_date):
-  br = Browser()
-  br.open("https://erp.iitkgp.ac.in/StudentPerformanceV2/auth/login.htm")
-  br.select_form(name="loginform")
-  br.form.set_all_readonly(False)
-  br["rollno"] = "16MA20051"
-  br["dob"] = single_date.strftime("%d-%m-%Y")
-  response = br.submit()
-  arr = response.readlines()
-  print '.'
-  if len(arr)!=330:
-    print single_date.strftime("%d-%m-%Y")
-    break
-  br.close()
+    response = br.submit()
+    if len(response.readlines())!=330:
+      flag = 0
+      print single_date.strftime("%d-%m-%Y")
+      break
+
+    br.close()
+
+    sys.stdout.write(next(spinner))
+    sys.stdout.flush()
+    sys.stdout.write('\b')
+
+  if flag:
+    print "Try another year."
+
+def main():
+  rollno = raw_input("Enter the rollno of your best friend.: ")
+  year   = input("Enter the possible birth year.       : ")
+  
+  start_date = date(year, 1, 1)
+  end_date   = date(year, 12, 31)
+
+  get_bday(rollno, start_date, end_date)
+
+if __name__ == '__main__':
+  main()
