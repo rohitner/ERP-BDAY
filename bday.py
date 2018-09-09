99d5a8710c00249439ad723f09454bba5a5bcfb4
Rohit Ner <rohitner1@gmail.com>
2018-09-09 11:36:46 +0530

    soft reset later

--- bday.py
+++ bday.py
@@ -1,6 +1,7 @@
 import sys
 from mechanize import Browser
 from datetime import timedelta, date
+from joblib import Parallel, delayed
 
 def spinning_cursor():
   while True:
@@ -11,29 +12,34 @@ def daterange(start_date, end_date):
     for n in range(int ((end_date - start_date).days)):
         yield start_date + timedelta(n)
 
+def request_bday(single_date, rollno, start_date, end_date):
+  br = Browser()
+  br.open("https://erp.iitkgp.ac.in/StudentPerformanceV2/auth/login.htm")
+  br.select_form(name="loginform")
+  br.form.set_all_readonly(False)
+
+  br["rollno"] = rollno
+  br["dob"] = single_date.strftime("%d-%m-%Y")
+
+  response = br.submit()
+  if len(response.readlines())!=330:
+    flag = 0
+    print single_date.strftime("%d-%m-%Y")
+    br.close()
+    return flag
+
+  br.close()
+
+  # sys.stdout.write(next(spinner))
+  # sys.stdout.flush()
+  # sys.stdout.write('\b')
+  # return flag
+
 def get_bday(rollno, start_date, end_date):
   spinner = spinning_cursor()
   flag = 1
-  for single_date in daterange(start_date, end_date):
-    br = Browser()
-    br.open("https://erp.iitkgp.ac.in/StudentPerformanceV2/auth/login.htm")
-    br.select_form(name="loginform")
-    br.form.set_all_readonly(False)
-
-    br["rollno"] = rollno
-    br["dob"] = single_date.strftime("%d-%m-%Y")
-
-    response = br.submit()
-    if len(response.readlines())!=330:
-      flag = 0
-      print single_date.strftime("%d-%m-%Y")
-      break
-
-    br.close()
-
-    sys.stdout.write(next(spinner))
-    sys.stdout.flush()
-    sys.stdout.write('\b')
+  flag = Parallel(n_jobs=-1)(delayed(request_bday)(single_date, rollno, start_date, end_date) for single_date in daterange(start_date, end_date))
+    
 
   if flag:
     print "Try another year."
